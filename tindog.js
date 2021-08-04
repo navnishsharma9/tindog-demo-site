44ea57977f4f215d7fab77b3bcff42a6132be154
Navnish Sharma <navnishs97@outlook.com>
2021-08-04 13:37:28 +0530

    new commit

--- tindog.js
+++ tindog.js
@@ -3,9 +3,9 @@ const app = express();
 const path = require('path');
 const serveStatic = require('serve-static');
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 // app.use(express.static('node../images'));
-app.use(serveStatic(path.join(__dirname, 'public')));
+// app.use(serveStatic(path.join(__dirname, 'public')));
 // app.use(serveStatic(path.join(__dirname, 'images')));
 
 
